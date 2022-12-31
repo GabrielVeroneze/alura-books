acad67cf1ab0f41e2283fab6af36666848bbffd5
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2022-12-30 23:22:52 -0300

    atualização carrossel

--- assets/js/initialize-swiper.js
+++ assets/js/initialize-swiper.js
@@ -1,13 +1,31 @@
 const swiper = new Swiper('.swiper', {
+    // Objeto com parâmetros de reprodução automática
     autoplay: {
-        delay: 5000,
+        // Atraso entre as transições (em ms)
+        delay: 4000,
     },
+
+    // Objeto com parâmetros de paginação
     pagination: {
+        // String com seletor CSS ou elemento HTML do contêiner com paginação
         el: '.swiper-pagination',
+        // String com o tipo de paginação.
         type: 'bullets',
     },
-    slidesPerView: 3,
+
+    // O slide ativo será centralizado, não sempre no lado esquerdo
+    centeredSlides: true,
+
+    // Ativar o modo de loop contínuo
+    loop: true,
+
+    // Número de slides por exibição (slides visíveis ao mesmo tempo no contêiner do slider)
+    slidesPerView: 2.25,
+
+    // Duração da transição entre os slides (em ms)
     speed: 400,
-    spaceBetween: 10,
+
+    // Distância entre slides em px
+    spaceBetween: 11,
 });
 
